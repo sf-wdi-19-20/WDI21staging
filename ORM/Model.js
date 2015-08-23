cc8b65c98f7cb14a0ca3a0f5584641e832ec37d3
Justin Castilla <pxlperfection@gmail.com>
2015-08-23 12:59:01 -0700

    Added basic callback funcitonality - NEEDS EYES

--- ORM/Model.js
+++ ORM/Model.js
@@ -8,75 +8,89 @@ function Model(name){
 
 Model.prototype = {
 
-  create : function (object){
-    var object_container = {};
-    object_container._id = this._id++;
-    object_container._ts = Date.now();
-    object_container.data = object;
-    this.data.push(object_container);
-    return object_container;
-  },
+    create : function (object, calback){
+        var object_container = {};
+        object_container._id = this._id++;
+        object_container._ts = Date.now();
+        object_container.data = object;
+        this.data.push(object_container);
+        return calback(object_container);
+    },
 
-  find_by_id : function(object_id){
-    return this.data.filter( function (object) {
-      if(object._id === object_id){
-        return object;
-      }
-    });
-  },
+    find_by_id : function(object_id, calback){
+        var found;
+        this.data.forEach( function (object) {
+          if(object._id === object_id){
+            found = calback(object);
+          }
+        });
+        return found;
+    },
 
-  delete : function(object_id) {
-    var pos, marked;
-    this.data.forEach( function (object, index) {
-      if(object._id === object_id){
-        marked = object;
-        pos = index;
-      }
-    });
-    this.data.splice(pos,1);
-    return marked;
-  },
+    update : function(object_id, update_object, callback) {
+        var update;
+        this.data.forEach( function (object) {
+          if(object._id === object_id){
+            object.data = update_object;
+            update = object;
+          }
+        });
+        return update;
+      
+    },
 
-  update : function(object_id, update_object) {
-    var update;
-    this.data.forEach( function (object) {
-      if(object._id === object_id){
-        object.data = update_object;
-        update = object;
-      }
-    });
-    return update;
-  }
+    delete : function(object_id, callback) {
+        var pos, marked;
+        this.data.forEach( function (object, index) {
+          if(object._id === object_id){
+            marked = object;
+            pos = index;
+          }
+        });
+        this.data.splice(pos,1);
+        return callback(marked);
+    }
 }
 
 /* Instantiate Model object */
-//var user = new Model("user");
+var user = new Model("user");
 
 /* Create objects */
-// [
-//   {first_name: "Mister", last_name: "Robot"},
-//   {first_name: "Stanley", last_name: "Steemer"},
-//   {first_name: "Action", last_name: "Bronson"},
-//   {first_name: "Jillian", last_name: "Murphy"},
-//   {first_name: "Nichole", last_name: "DeJarden"}
-// ].forEach( function(person) {
-//   user.create(person);
-// });
+[
+  {first_name: "Mister", last_name: "Robot"},
+  {first_name: "Stanley", last_name: "Steemer"},
+  {first_name: "Action", last_name: "Bronson"},
+  {first_name: "Jillian", last_name: "Murphy"},
+  {first_name: "Nichole", last_name: "DeJarden"}
+].forEach( function(person) {
+  user.create(person, function(person){
+    return person;
+  });
+});
+
 
 /* Find object by id */
-//var found = user.find_by_id(3);
+var found = user.find_by_id(3, function(success) {
+    return success;
+});
 //console.log("Searched:\n", found);
 
-/* Delete object */
-//var deleted = user.delete(1);
 
-/* Create object, Show continuation of id scheme after delete*/
-//user.create({first_name: "Billy", last_name: "Bragg"});
-//console.log("Deleted:\n", deleted);
+/* Delete object */
+var deleted = user.delete(1, function(success) {
+    return success;
+});
 
 /* Update object properties */
-//var updated = user.update(1, {first_name: "Joey", last_name: "Michaels"});
-//console.log("Updated:\n", updated);
+
+var updated = user.update(2, {first_name: "Joey", last_name: "Michaels"});
+
+
+/* Create object, Show continuation of id scheme after delete*/
+user.create({first_name: "Billy", last_name: "Bragg"}, function(person){
+    return person;
+  });
+
 
 /* Display data content */
-//console.log(user);
+console.log(user.data);
